0d8f564e9c039f6a3584e836f38727f88b256f3f
JR Tashjian <jrtashjian@gmail.com>
2018-11-14 10:09:40 -0500

    Minor code cleanup.

--- cfs-ninja-forms-field.php
+++ cfs-ninja-forms-field.php
@@ -24,11 +24,12 @@ class cfs_ninja_forms_field extends cfs_field {
 		<select name="<?php esc_attr( $field->input_name ); ?>" name="<?php esc_attr( $field->input_class ); ?>">
 			<option value="0">None</option>
 
-			<?php // Output available forms as options
+			<?php
+			// Output available forms as options
 			foreach ( $ninja_forms_listing as $form ) :
-				$form_id = $form->get_id();
+				$form_id    = $form->get_id();
 				$form_title = $form->get_setting( 'title' );
-				$selected = in_array( $form_id, (array) $field->value, true ) ? ' selected' : '';
+				$selected   = in_array( $form_id, (array) $field->value, true ) ? ' selected' : '';
 				?>
 
 				<option value="<?php echo esc_attr( $form_id ); ?>"<?php echo esc_attr( $selected ); ?>>
